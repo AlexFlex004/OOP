bdb75b0d0a14cf64e5189fceb8d6c17336c35aa8
Himawari Lad <Sacrificial.animal@gmail.com>
2025-08-29 12:12:17 +0300

    Code added 3

--- src/org/skypro/skyshop/Searchable.java
+++ src/org/skypro/skyshop/Searchable.java
@@ -6,7 +6,7 @@ public interface Searchable {
     String getSearchTerm();
     String getContentType();
     String getName();
-    Spring[] getTags();
+    String[] getTags();
 
     default String getStringRepresentation() {
         return getName() + " — " + getContentType();
